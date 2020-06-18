973fd4eae5abf243082d10f9c405aed963da8e2d
Andrey Beryukhov <beryukhov-andrey@yandex.ru>
2020-06-19 00:09:37 +0300

    Add Coffee item

--- app/src/main/java/ru/beryukhov/coffeegram/MainActivity.kt
+++ app/src/main/java/ru/beryukhov/coffeegram/MainActivity.kt
@@ -1,16 +1,18 @@
 package ru.beryukhov.coffeegram
 
 import android.os.Bundle
+import androidx.annotation.DrawableRes
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.Composable
-import androidx.ui.core.ContentScale
-import androidx.ui.core.Modifier
-import androidx.ui.core.clip
-import androidx.ui.core.setContent
+import androidx.ui.core.*
 import androidx.ui.foundation.Image
 import androidx.ui.foundation.Text
 import androidx.ui.foundation.shape.corner.RoundedCornerShape
 import androidx.ui.layout.*
+import androidx.ui.layout.RowScope.gravity
+import androidx.ui.material.Button
+import androidx.ui.material.OutlinedButton
+import androidx.ui.material.TextButton
 import androidx.ui.res.imageResource
 import androidx.ui.text.style.TextOverflow
 import androidx.ui.tooling.preview.Preview
@@ -22,9 +24,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            CoffeegramTheme {
-                CoffeeList()
-            }
+            DefaultPreview()
         }
     }
 }
@@ -59,10 +59,64 @@ fun CoffeeList() {
     }
 }
 
+data class CoffeeType(
+    @DrawableRes
+    val image: Int,
+    val name: String,
+    val count: Int = 0
+)
+
+@Composable
+fun CoffeeTypeItem(type: CoffeeType) {
+    Row(
+        modifier = Modifier.padding(16.dp)
+    ) {
+        Image(
+            imageResource(type.image), modifier = Modifier
+                .preferredHeightIn(maxHeight = 48.dp)
+                .preferredWidthIn(maxWidth = 48.dp)
+                .fillMaxWidth()
+                .clip(shape = RoundedCornerShape(24.dp))
+                .gravity(Alignment.CenterVertically),
+            contentScale = ContentScale.Crop
+        )
+        Spacer(Modifier.preferredWidth(16.dp))
+        Text(
+            type.name, style = typography.body1,
+            modifier = Modifier.gravity(Alignment.CenterVertically).weight(1f)
+        )
+        Row(modifier = Modifier.gravity(Alignment.CenterVertically)) {
+            Spacer(Modifier.preferredWidth(16.dp))
+            val textButtonModifier = Modifier.gravity(Alignment.CenterVertically)
+                .preferredSizeIn(
+                    maxWidth = 32.dp,
+                    maxHeight = 32.dp,
+                    minWidth = 0.dp,
+                    minHeight = 0.dp
+                )
+            TextButton(onClick = { }, padding = InnerPadding(0.dp), modifier = textButtonModifier) {
+                Text("-")
+            }
+            Text(
+                type.count.toString(), style = typography.body2,
+                modifier = Modifier.gravity(Alignment.CenterVertically)
+            )
+            TextButton(
+                onClick = { },
+                padding = InnerPadding(0.dp),
+                modifier = textButtonModifier
+            ) {
+                Text("+")
+            }
+        }
+    }
+}
+
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     CoffeegramTheme {
-        CoffeeList()
+        //CoffeeList()
+        CoffeeTypeItem(CoffeeType(R.drawable.header, "Cappucino"))
     }
 }
