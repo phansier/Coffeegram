4d0bbb2b09da5d3b966767123550365b0c950cf9
Andrey Beryukhov <beryukhov-andrey@yandex.ru>
2020-11-23 20:33:41 +0300

    Migrate to Kotlin DSL

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Coffeegram
 
 [![Compose Version](https://img.shields.io/badge/Jetpack%20Compose-1.0.0--alpha07-yellow)](https://developer.android.com/jetpack/compose)
-[![Kotlin Version](https://img.shields.io/badge/Kotlin-1.4.10-blue.svg)](https://kotlinlang.org)
+[![Kotlin Version](https://img.shields.io/badge/Kotlin-1.4.20-blue.svg)](https://kotlinlang.org)
 
 Android app using Jetpack Compose together with StateFlow and MVI
 
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,17 +1,16 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 buildscript {
-    ext {
-        compose_version = '1.0.0-alpha07'
-        kotlin_version = "1.4.10"
-        coroutines_version = "1.4.0"
-    }
+    val compose_version by extra("1.0.0-alpha07")
+    val kotlin_version by extra("1.4.20")
+    val coroutines_version by extra("1.4.0")
+
     repositories {
         google()
         jcenter()
     }
     dependencies {
-        classpath 'com.android.tools.build:gradle:4.2.0-alpha15'
-        classpath "org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version"
+        classpath("com.android.tools.build:gradle:4.2.0-alpha15")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version")
 
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
@@ -25,6 +24,6 @@ allprojects {
     }
 }
 
-task clean(type: Delete) {
-    delete rootProject.buildDir
+task<Delete>("clean") {
+    delete(rootProject.buildDir)
 }
