12954adc7fd3d77e5bde2c8a164e483ea3be250c
Andrey Beryukhov <beryukhov-andrey@yandex.ru>
2021-04-06 20:09:36 +0300

    Update to beta-03

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,15 +1,15 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 buildscript {
-    val compose_version by extra("1.0.0-beta02")
+    val compose_version by extra("1.0.0-beta03")
     val kotlin_version by extra("1.4.31")
-    val coroutines_version by extra("1.4.0")
+    val coroutines_version by extra("1.4.3")
 
     repositories {
         google()
         jcenter()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:7.0.0-alpha10")
+        classpath("com.android.tools.build:gradle:7.0.0-alpha12")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version")
 
         // NOTE: Do not place your application dependencies here; they belong
