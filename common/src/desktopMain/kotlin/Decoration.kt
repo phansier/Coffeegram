89026eabb0593d380abae12d67d879384b58cc85
Andrey Beryukhov <beryukhov-andrey@yandex.ru>
2021-06-10 19:03:23 +0300

    Fix compile & remove unused

--- common/src/desktopMain/kotlin/Decoration.kt
+++ common/src/desktopMain/kotlin/Decoration.kt
@@ -1,41 +1,8 @@
 
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.res.imageResource
 import java.awt.image.BufferedImage
 import javax.imageio.ImageIO
 
-@Composable
-fun icEmpty() = imageResource("images/empty.png")
 
-@Composable
-fun icBack() = imageResource("images/back.png")
-
-@Composable
-fun icRefresh() = imageResource("images/refresh.png")
-
-@Composable
-fun icDots() = imageResource("images/dots.png")
-
-@Composable
-fun icFilterGrayscaleOn() = imageResource("images/grayscale_on.png")
-
-@Composable
-fun icFilterGrayscaleOff() = imageResource("images/grayscale_off.png")
-
-@Composable
-fun icFilterPixelOn() = imageResource("images/pixel_on.png")
-
-@Composable
-fun icFilterPixelOff() = imageResource("images/pixel_off.png")
-
-@Composable
-fun icFilterBlurOn() = imageResource("images/blur_on.png")
-
-@Composable
-fun icFilterBlurOff() = imageResource("images/blur_off.png")
-
-@Composable
-fun icFilterUnknown() = imageResource("images/filter_unknown.png")
 
 private var icon: BufferedImage? = null
 fun icAppRounded(): BufferedImage {
