b2f3a5b07e5ffd3596675945b428b825792588a2
Andrey Beryukhov <beryukhov-andrey@yandex.ru>
2021-06-18 00:14:38 +0300

    Update to beta-09

--- app/src/main/java/ru/beryukhov/coffeegram/animations/SplashTransition.kt
+++ app/src/main/java/ru/beryukhov/coffeegram/animations/SplashTransition.kt
@@ -1,19 +1,17 @@
 package ru.beryukhov.coffeegram.animations
 
-import androidx.compose.animation.core.*
-import androidx.compose.foundation.layout.Box
+import androidx.compose.animation.core.MutableTransitionState
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateDp
+import androidx.compose.animation.core.animateFloat
+import androidx.compose.animation.core.spring
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.core.updateTransition
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.alpha
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
-import ru.beryukhov.coffeegram.PagesContent
-import ru.beryukhov.coffeegram.model.DaysCoffeesStore
-import ru.beryukhov.coffeegram.model.NavigationStore
-import ru.beryukhov.coffeegram.pages.LandingPage
 
 enum class SplashState { Shown, Completed }
 
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 buildscript {
-    val compose_version by extra("1.0.0-beta08")
+    val compose_version by extra("1.0.0-beta09")
     val kotlin_version by extra("1.5.10")
     val coroutines_version by extra("1.5.0")
 
@@ -9,7 +9,7 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:7.1.0-alpha01")
+        classpath("com.android.tools.build:gradle:7.1.0-alpha02")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version")
 
         // NOTE: Do not place your application dependencies here; they belong
