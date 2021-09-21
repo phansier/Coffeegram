bdbbbbd93a5b5dd5d84c4d5bd3a2218c7cb83f38
Andrey Beryukhov <beryukhov-andrey@yandex.ru>
2021-09-21 16:36:52 +0300

    Update to 1.1.0-alpha04

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 buildscript {
-    val compose_version by extra("1.1.0-alpha02")
-    val kotlin_version by extra("1.5.21")
-    val coroutines_version by extra("1.5.1-native-mt")
+    val compose_version by extra("1.1.0-alpha04")
+    val kotlin_version by extra("1.5.30")
+    val coroutines_version by extra("1.5.2-native-mt")
     val realm_version by extra("0.5.0")
     val datastore_version by extra("1.0.0")
 
@@ -11,7 +11,7 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:7.1.0-alpha10")
+        classpath("com.android.tools.build:gradle:7.1.0-alpha12")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version")
         classpath("io.realm.kotlin:gradle-plugin:$realm_version")
     }
