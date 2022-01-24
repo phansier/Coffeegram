279890968d625c72ab9ebb7489143677583a5339
Andrey Beryukhov <beryukhov-andrey@yandex.ru>
2022-01-24 19:13:56 +0300

    Add more drinks

--- app-wear-common/src/main/kotlin/ru/beryukhov/coffeegram/data/CoffeeType.kt
+++ app-wear-common/src/main/kotlin/ru/beryukhov/coffeegram/data/CoffeeType.kt
@@ -8,7 +8,17 @@ enum class CoffeeType(
     @StringRes val nameId: Int,
     @DrawableRes val iconId: Int = R.drawable.coffee
 ) {
-    Cappuccino(R.string.cappuccino, R.drawable.cappucino),
+    Cappuccino(R.string.cappuccino, R.drawable.cappuccino),
     Latte(R.string.latte, R.drawable.latte),
-    Americano(R.string.americano),
+    Americano(R.string.americano, R.drawable.americano),
+    Macchiato(R.string.macchiato, R.drawable.macchiato),
+    Glace(R.string.glace, R.drawable.glace),
+    Frappe(R.string.frappe, R.drawable.latte),
+    Espresso(R.string.espresso, R.drawable.espresso),
+    Mocha(R.string.mocha, R.drawable.mocha),
+    Fredo(R.string.fredo, R.drawable.fredo),
+    Irish(R.string.irish, R.drawable.irish),
+    Cocoa(R.string.cocoa, R.drawable.cocoa),
+    Chocolate(R.string.chocolate, R.drawable.chocolate),
+    //icons from here: https://www.freepik.com/free-vector/list-different-types-coffee_951047.htm
 }
