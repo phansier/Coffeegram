8f861896b7f8a85e63c281ed724146ee09fd8091
Andrei Beriukhov <beryukhov-andrey@yandex.ru>
2024-05-21 11:00:43 +0000

    Custom marker UI

--- app/src/main/java/ru/beryukhov/coffeegram/pages/MapPage.kt
+++ app/src/main/java/ru/beryukhov/coffeegram/pages/MapPage.kt
@@ -6,29 +6,50 @@ import android.Manifest
 import android.content.Context
 import android.content.pm.PackageManager
 import android.location.LocationManager
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
-import com.google.maps.android.compose.AdvancedMarker
 import com.google.maps.android.compose.GoogleMap
 import com.google.maps.android.compose.MapProperties
+import com.google.maps.android.compose.MarkerComposable
 import com.google.maps.android.compose.MarkerState
 import com.google.maps.android.compose.rememberCameraPositionState
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -39,7 +60,6 @@ import ru.beryukhov.coffeegram.R
 import ru.beryukhov.coffeegram.repository.CoffeeShop
 import ru.beryukhov.coffeegram.repository.coffeeShops
 
-@Preview
 @Composable
 internal fun MapPagePreview() {
     Column {
@@ -82,15 +102,28 @@ fun ColumnScope.MapPage(modifier: Modifier = Modifier) {
                 properties = MapProperties().copy(isMyLocationEnabled = true),
                 cameraPositionState = cameraPositionState
             ) {
+                val zoom by remember { mutableFloatStateOf(cameraPositionState.position.zoom) }
                 coffeeShops.forEach {
-                    AdvancedMarker(
-                        state = MarkerState(position = LatLng(it.latitude, it.longitude)),
-                        title = it.name
-                    )
+                    var state by mutableStateOf(ExtendedMarkerState(
+                        position = LatLng(it.latitude, it.longitude),
+                        highlighted = false,
+                        expanded = zoom >= 11f
+                    ))
+                    MarkerComposable(
+                        state = state.markerState,
+                        ) {
+                        Marker(
+                            name = it.name,
+                            highlighted = state.highlighted,
+                            expanded = cameraPositionState.position.zoom >= 11f,
+                            onClick = { state = state.copy(highlighted = !state.highlighted) })
+
+                    }
                 }
+
             }
-            // Text(text = cameraPositionState.position.target.toString(), modifier = Modifier.align(Alignment.Center))
         }
+
     } else {
         Box(
             modifier = modifier.fillMaxSize(),
@@ -99,6 +132,70 @@ fun ColumnScope.MapPage(modifier: Modifier = Modifier) {
         }
 
     }
+}
+
+data class ExtendedMarkerState(
+    val position: LatLng,
+    val highlighted: Boolean,
+    val expanded: Boolean
+) {
+    val markerState: MarkerState get() = MarkerState(position)
+}
+
+
+@Composable
+@Preview
+fun Marker(name: String = "Title", descr: String = "Subtitle", highlighted: Boolean = false, expanded: Boolean = true, onClick: () -> Unit = { }) =
+    Row(
+    verticalAlignment = Alignment.CenterVertically,
+    modifier = Modifier
+        .padding(16.dp)
+        .shadow(elevation = 9.dp, shape = RoundedCornerShape(size = 6.dp))
+        .height(if (expanded) 44.dp else 30.dp)
+        .background(
+            color = if (highlighted) Color(0xFFE8E5E3) else Color(0xFFFFFFFF),
+            shape = RoundedCornerShape(size = 6.dp)
+        )
+        .padding(start = 4.dp, top = 3.dp, end = 8.dp, bottom = 3.dp)
+        .clickable { onClick() }
+) {
+    Image(
+        painter = painterResource(id = R.drawable.logo_splash),
+        contentDescription = "image description",
+        contentScale = ContentScale.Fit,
+        modifier = Modifier
+            .padding(start = 0.dp, top = 1.dp, end = 2.dp, bottom = 1.dp)
+            .width(18.dp)
+            .height(18.dp)
+    )
+    Column(
+        verticalArrangement = Arrangement.spacedBy(-4.dp, Alignment.CenterVertically),
+        horizontalAlignment = Alignment.Start,
+        modifier = Modifier.padding(start = 4.dp)
+    ) {
+        val textColor = /*Color(0xFF003DD9)*/ MaterialTheme.colorScheme.onPrimaryContainer
+        Text(
+            text = name,
+            style = TextStyle(
+                fontSize = 17.sp,
+                lineHeight = 24.sp,
+                fontWeight = FontWeight(350),
+                color = textColor,
+            )
+        )
+        if (expanded) {
+            Text(
+                text = descr,
+                style = TextStyle(
+                    fontSize = 13.sp,
+                    lineHeight = 18.sp,
+                    fontWeight = FontWeight(350),
+                    color = textColor,
+                )
+            )
+        }
+    }
+
 }
 
 @Composable
