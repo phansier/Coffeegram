56a25f6fb4076cd8aad004671548020d1512a029
Andrei Beriukhov <beriukhov@gmail.com>
2025-02-11 17:10:28 +0200

    New datastore

--- app/src/main/java/ru/beryukhov/coffeegram/repository/ThemePreferencesSerializer.kt
+++ app/src/main/java/ru/beryukhov/coffeegram/repository/ThemePreferencesSerializer.kt
@@ -2,7 +2,7 @@ package ru.beryukhov.coffeegram.repository
 
 import androidx.datastore.core.CorruptionException
 import androidx.datastore.core.Serializer
-import androidx.datastore.preferences.protobuf.InvalidProtocolBufferException
+import com.google.protobuf.InvalidProtocolBufferException
 import java.io.InputStream
 import java.io.OutputStream
 
