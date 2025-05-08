e09536d8299ee73da709ba21bbda1f94efc44815
Andrei Beriukhov <beriukhov@gmail.com>
2025-05-08 15:34:22 +0300

    Downgrade koin back

--- app/src/main/java/ru/beryukhov/coffeegram/PagesContent.kt
+++ app/src/main/java/ru/beryukhov/coffeegram/PagesContent.kt
@@ -13,7 +13,6 @@ import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
