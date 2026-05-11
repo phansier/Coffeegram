2194a528e44e2c13e26de66835fb6964526f016b
Andrei Beriukhov <beriukhov@gmail.com>
2026-05-11 12:46:48 +0300

    complex screenshots still not working in CI

--- app/src/screenshotTest/kotlin/Screenshots.kt
+++ app/src/screenshotTest/kotlin/Screenshots.kt
@@ -1,22 +1,15 @@
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.android.tools.screenshot.PreviewTest
 import kotlinx.collections.immutable.persistentMapOf
 import kotlinx.datetime.LocalDate
 import kotlinx.datetime.Month
-import ru.beryukhov.coffeegram.app_ui.PreviewTheme
 import ru.beryukhov.coffeegram.data.CoffeeType
-import ru.beryukhov.coffeegram.data.CoffeeTypes.Cappuccino
 import ru.beryukhov.coffeegram.data.Picture
 import ru.beryukhov.coffeegram.data.PrintableText
 import ru.beryukhov.coffeegram.view.CoffeeTypeItem
 import ru.beryukhov.coffeegram.view.MonthTable
-import ru.beryukhov.coffeegram.view.SampleTable
 import ru.beryukhov.date_time_utils.YearMonth
 
 /**
@@ -27,19 +20,23 @@ import ru.beryukhov.date_time_utils.YearMonth
 @Preview
 @Preview(name = "Large preview", widthDp = 200)
 @Composable
-private fun Preview() = PreviewTheme {
-    var count by remember { mutableIntStateOf(5) }
-    CoffeeTypeItem(
-        coffeeType = Cappuccino,
-        count = count,
-        onIncrement = { count++ },
-        onDecrement = { count-- }
-    )
+private fun Preview() {
+    CoffeeTypeItem(object : CoffeeType {
+        override val localizedName: PrintableText = PrintableText("Coffeegram")
+        override val icon: Picture = Picture.EMPTY
+        override val dbKey: String = "Cappuccino"
+    }, 5, {}, {})
 }
 
 @PreviewTest
 @Preview(showBackground = true)
 @Composable
-internal fun TablePreview() = PreviewTheme {
-    SampleTable()
+internal fun TablePreview() {
+    MonthTable(
+        yearMonth = YearMonth(2020, Month(7)),
+        today = LocalDate(2020, 7, 14),
+        filledDayItemsMap = persistentMapOf(),
+        onClick = {},
+        modifier = Modifier,
+    )
 }
