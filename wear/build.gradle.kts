f4c78daf06aed698e9cbad331d3bcbcab04639c3
Andrei Beriukhov <beriukhov@gmail.com>
2026-06-12 11:47:05 +0300

    Fix wear autopublish

--- wear/build.gradle.kts
+++ wear/build.gradle.kts
@@ -113,7 +113,7 @@ object KeyHelper {
 
 play {
     serviceAccountCredentials.set(file("../play_config.json"))
-    track.set("Alpha")
+    track.set("wear:alpha")
     defaultToAppBundles.set(true)
     // Phone and wear publish into a single Play edit; commit once after both upload.
     commit.set(false)
