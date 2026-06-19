999230e2f3d79faf2ac01b9dbc3198d3f2f0bd7f
Andrei Beriukhov <beriukhov@gmail.com>
2026-06-19 16:26:13 +0300

    Logger

--- cmp-common/src/commonMain/kotlin/ru/beryukhov/coffeegram/repository/MapCoffeesRequest.kt
+++ cmp-common/src/commonMain/kotlin/ru/beryukhov/coffeegram/repository/MapCoffeesRequest.kt
@@ -4,6 +4,7 @@ import io.ktor.client.HttpClient
 import io.ktor.client.call.body
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.client.plugins.logging.LogLevel
+import io.ktor.client.plugins.logging.Logger
 import io.ktor.client.plugins.logging.Logging
 import io.ktor.client.request.get
 import io.ktor.client.request.header
@@ -25,8 +26,17 @@ private val client = HttpClient {
     if (LOG_HTTP) {
         install(Logging) {
             level = LogLevel.ALL
+            logger = object : Logger {
+                override fun log(message: String) {
+                    Log.d("HTTP", message)
+                }
+            }
         }
     }
+}
+
+expect object Log {
+    fun d(tag: String, message: String)
 }
 
 suspend fun coffeeShops(): List<CoffeeShop> = try {
