7526de2c7adbe737ce9d729865d19263dd9f5982
Andrei Beriukhov <beriukhov@gmail.com>
2026-09-03 15:21:53 +0300

    providePreview + previewSizeMode

--- app/src/main/java/ru/beryukhov/coffeegram/widget/FirstGlanceWidget.kt
+++ app/src/main/java/ru/beryukhov/coffeegram/widget/FirstGlanceWidget.kt
@@ -37,6 +37,7 @@ import androidx.glance.action.actionParametersOf
 import androidx.glance.action.actionStartActivity
 import androidx.glance.action.clickable
 import androidx.glance.appwidget.GlanceAppWidget
+import androidx.glance.appwidget.PreviewSizeMode
 import androidx.glance.appwidget.SizeMode
 import androidx.glance.appwidget.components.Scaffold
 import androidx.glance.appwidget.lazy.LazyColumn
@@ -63,6 +64,8 @@ import androidx.glance.unit.ColorProvider
 import kotlinx.collections.immutable.PersistentList
 import kotlinx.collections.immutable.persistentListOf
 import kotlinx.collections.immutable.toPersistentList
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.withTimeoutOrNull
 import org.jetbrains.compose.resources.PreviewContextConfigurationEffect
 import org.koin.core.component.KoinComponent
 import org.koin.core.component.inject
@@ -75,6 +78,7 @@ import ru.beryukhov.coffeegram.model.NavigationConstants.TODAYS_COFFEE_LIST
 import ru.beryukhov.coffeegram.widget.FirstGlanceWidget.Companion.BIG_SQUARE
 import ru.beryukhov.coffeegram.widget.FirstGlanceWidget.Companion.HORIZONTAL_RECTANGLE
 import kotlin.math.roundToInt
+import kotlin.time.Duration.Companion.seconds
 import ru.beryukhov.compose_common.R as common_R
 
 class FirstGlanceWidget : GlanceAppWidget(errorUiLayout = R.layout.layout_widget_custom_error), KoinComponent {
@@ -83,6 +87,9 @@ class FirstGlanceWidget : GlanceAppWidget(errorUiLayout = R.layout.layout_widget
     // for Android 12 Responsive layouts feature
     override val sizeMode: SizeMode =
         SizeMode.Responsive(setOf(SMALL_SQUARE, HORIZONTAL_RECTANGLE, BIG_SQUARE))
+
+    override val previewSizeMode: PreviewSizeMode =
+        SizeMode.Responsive(setOf(HORIZONTAL_RECTANGLE, BIG_SQUARE))
 
     override suspend fun provideGlance(context: Context, id: GlanceId) {
         val dataBridge: WidgetDataBridge by inject()
@@ -96,7 +103,24 @@ class FirstGlanceWidget : GlanceAppWidget(errorUiLayout = R.layout.layout_widget
         }
     }
 
+    /**
+     * The store starts empty and loads from storage asynchronously, so wait a bounded time for
+     * a day with any coffee in it. Falling through covers both "still loading" and "nothing
+     * logged today", and either way a sample reads better in the picker than an empty widget.
+     */
+    override suspend fun providePreview(context: Context, widgetCategory: Int) {
+        val dataBridge: WidgetDataBridge by inject()
+        val coffees = withTimeoutOrNull(STORED_DATA_TIMEOUT) {
+            dataBridge.getCurrentDayList().first { day -> day.any { it.count > 0 } }
+        } ?: widgetPreviewCounts.toWidgetCoffees()
+        provideContent {
+            WidgetContent(coffees = coffees, increment = {}, decrement = {})
+        }
+    }
+
     companion object {
+        private val STORED_DATA_TIMEOUT = 1.seconds
+
         // TODO check this sizes after UI implementation
         // https://developer.android.com/develop/ui/views/appwidgets/layouts#anatomy_determining_size
         internal val SMALL_SQUARE = DpSize(50.dp, 50.dp) // 1x1
